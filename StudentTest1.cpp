e1081c412e7b103203c8404b15452f47b23d5874
Yevgeny Ivanov <119947522+yevgenyivanov@users.noreply.github.com>
2023-04-13 18:08:20 +0300

    corrected tests

--- StudentTest1.cpp
+++ StudentTest1.cpp
@@ -36,25 +36,23 @@ TEST_CASE("The card scheme at the end of the game")
     Game game(p1, p2);
     game.playAll();
     int sum = p1.stacksize() + p1.cardesTaken() + p2.stacksize() + p2.cardesTaken();
-    game.printLog();
-    game.printStats();
     CHECK(sum == 52);
 }
 
-// TEST_CASE("Throwing errors from the functions")
-// {
-//     Player p1("Alice");
-//     Player p2("Bob");
-//     Game game(p1, p2);
-//     for (int i = 0; i < 5; i++)
-//     {
-//         game.playTurn();
-//     }
-//     CHECK_NOTHROW(game.printLastTurn());
-//     CHECK_NOTHROW(game.printLog());
-//     CHECK_NOTHROW(game.printStats());
-//     CHECK_NOTHROW(game.printWiner());
-// }
+TEST_CASE("Throwing errors from the functions")
+{
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+    for (int i = 0; i < 5; i++)
+    {
+        game.playTurn();
+    }
+    CHECK_NOTHROW(game.printLastTurn());
+    CHECK_NOTHROW(game.printLog());
+    CHECK_NOTHROW(game.printStats());
+    CHECK_NOTHROW(game.printWiner());
+}
 
 TEST_CASE("Activating another turn after the game is over")
 {
